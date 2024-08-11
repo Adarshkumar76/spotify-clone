1022ddb7a73579eee107952a3d018dc3b2f36ea6
adarsh kumar singh <adarsh@kali>
2024-08-11 19:32:53 +0530

    local link remove & portfolio link add

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let currfolder;
 
 async function getSongs(folder) {
   currfolder = folder;
-  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -56,7 +56,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-  let a = await fetch(`http://127.0.0.1:5500/songs/`);
+  let a = await fetch(`/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -69,7 +69,7 @@ async function displayAlbums() {
     if (e.href.includes("/songs/")) {
       let folder = e.href.split("/").slice(-1)[0];
 
-      let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
+      let a = await fetch(`/songs/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
         <div  class="play">
